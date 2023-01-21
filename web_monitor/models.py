2d62cc65c8e369c2143583ec413226598fa24851
Seshien <maciek22999@wp.pl>
2023-01-21 15:48:46 +0100

    wip

--- web_monitor/models.py
+++ web_monitor/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Result(models.Model):
-    URL = models.CharField(max_length=1024)
+    ID = models.TextField(unique=True)
     created_on = models.DateTimeField(auto_now=True, auto_created=True)
-    sis = models.FloatField()
-    lcps = models.FloatField()
+    data_json = models.TextField(null=True)
+    finished = models.BooleanField(default=False)
--- web_monitor/views.py
+++ web_monitor/views.py
@@ -6,7 +6,21 @@ from django.shortcuts import render, HttpResponse, redirect
 from benchmark.logic.BenchmarkRunner import LogicModule
 from web_monitor.models import Result
 from .utils import get_plot
+import threading
+import datetime
 
+def run_thread(args, ID = "id_test"):
+    r = Result()
+    r.ID=ID
+    r.created_on=datetime.datetime.now()
+    r.save()
+    lm = LogicModule(args)
+    lm.run()
+
+    r.data_json = json.dumps(lm.data)
+    r.finished = True
+
+    r.save()
 
 def home(request):
     if request.method == "POST":
@@ -22,32 +36,27 @@ def home(request):
                    }
         print(context)
 
-        args = {
+        run_arguments = {
             "urls": [url],
             "amount_of_rounds": rounds,
             "seconds_between_rounds": int(interval),
 
         }
-        lm = LogicModule(args)
-        lm.run()
+        threading.Thread(target=run_thread, args=[run_arguments]).start()
 
-        for url, results in lm.data.items():
-            for i in range(rounds):
-                for y in range(2):
-                    r = Result()
-                    r.URL = url
-                    r.sis = results["sis"][i][y]
-                    r.lcps = results["lcps"][i][y]
-                    r.save()
+        return HttpResponse("Hello, BENCHMARK")
 
-        history = Result.objects.filter(URL=url).order_by("created_on").all()
-        x = [r.created_on for r in history]
-        y = [r.sis for r in history]
-        z = [r.lcps for r in history]
-        chart = get_plot(x, y, z)
-        return render(request, 'plot.html', {'chart': chart})
 
-        # json_str = json.dumps(lm.data)
+        #
 
     if request.method == "GET":
         return render(request, 'base.html')
+
+def results(request):
+    url = request.data.id
+    history = Result.objects.filter(URL=url).order_by("created_on").all()
+    x = [r.created_on for r in history]
+    y = [r.sis for r in history]
+    z = [r.lcps for r in history]
+    chart = get_plot(x, y, z)
+    return render(request, 'plot.html', {'chart': chart})
