fa222312f2139c25cbb91cc06a03697bddb667f9
Seshien <maciek22999@wp.pl>
2023-01-25 19:18:06 +0100

    changed plots

--- web_monitor/utils.py
+++ web_monitor/utils.py
@@ -22,6 +22,9 @@ def get_plot(labels, sis, lcps):
     width = 0.35
     plt.style.use('seaborn-darkgrid')
     fig, ax = plt.subplots()
+    #labels are rounds now, sis, lcps
+    #for each round, we need to get every URL
+    #
     rects1 = ax.bar(x - width / 2, sis, width, label='Search Index')
     rects2 = ax.bar(x + width / 2, lcps, width, label='Largest Contentful Paint')
     ax.set_ylabel('Time [ms]')
@@ -30,6 +33,29 @@ def get_plot(labels, sis, lcps):
     ax.legend()
     ax.bar_label(rects1, padding=3)
     ax.bar_label(rects2, padding=3)
+    fig.tight_layout()
+    graph = get_graph()
+    return graph
+
+def get_other_plot(labels, urls, title):
+
+    width = 0.2
+    plt.style.use('seaborn-darkgrid')
+    fig, ax = plt.subplots()
+    #for each round, we need to get every URL
+    #for every url, we need to make round list
+
+    ax.set_ylabel('Time [ms]')
+    ax.set_title(title)
+
+    x = np.arange(len(labels))
+
+
+    for number, (url, rounds) in enumerate(urls.items()):
+        rects = ax.bar(x - width + width * number, rounds, width, label=url)
+        ax.bar_label(rects, padding=3)
+    ax.set_xticks(x, labels)
+    ax.legend()
     fig.tight_layout()
     graph = get_graph()
     return graph
